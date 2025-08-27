da21493137c14d21429f0f735a151c899c91f74c
ahmed-subhan <syedahmedsubhanr9@gmail.com>
2025-08-27 19:11:35 +0500

    commit

--- image annotation.py
+++ image annotation.py
@@ -28,3 +28,35 @@ center1_x = top_left1[0] + rect1_width // 2
 center1_y = top_left1[1] + rect1_hight // 2
 center2_x = top_left2[0] + rect2_width //2
 center2_y = top_left2[1] + rect2_hight // 2
+cv2.circle(image_rgb, (center1_x, center1_y), 15, (0, 255, 0), -1)
+cv2.circle(image_rgb, (center2_x, center2_y), 15, (0, 0, 255), -1)
+
+
+cv2.line(image_rgb, (center1_x, center1_y), (center2_x, center2_y), (0, 255, 0), 3)
+
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+cv2.putText(image_rgb, 'Region 1', (top_left1[0], top_left1[1] - 10), font, 0.7, (0, 255, 255), 2, cv2.LINE_AA)
+cv2.putText(image_rgb, 'Region 2', (top_left2[0], top_left2[1] - 10), font, 0.7, (255, 0, 255), 2, cv2.LINE_AA)
+cv2.putText(image_rgb, 'Center 1', (center1_x - 40, center1_y + 40), font, 0.6, (0, 255, 0), 2, cv2.LINE_AA)
+cv2.putText(image_rgb, 'Center 2', (center2_x - 40, center2_y + 40), font, 0.6, (0, 0, 255), 2, cv2.LINE_AA)
+
+
+arrow_start = (width - 50, 20)
+arrow_end = (width - 50, hight - 20)
+
+
+cv2.arrowedLine(image_rgb, arrow_start, arrow_end, (255, 255, 0), 3, tipLength=0.05)
+cv2.arrowedLine(image_rgb, arrow_end, arrow_start, (255, 255, 0), 3, tipLength=0.05)
+
+
+
+hight_label_position = (arrow_start[0] - 150, (arrow_start[1] + arrow_end[1]) // 2)
+cv2.putText(image_rgb, f'Hight: {hight}px', hight_label_position, font, 0.8, (255, 255, 0), 2, cv2.LINE_AA)
+
+
+plt.figure(figsize=(12, 8))
+plt.imshow(image_rgb)
+plt.title('Annotated image with regions, centers, and bi-directional hight arrow')
+plt.axis('off')
+plt.show()
